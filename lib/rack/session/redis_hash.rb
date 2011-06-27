e8b87679e90018da58fff7f0b30bf67468268168
Carl Zulauf <czulauf@lyconic.com>
2011-06-27 18:13:59 -0500

    Improved handing of session renewals.

--- lib/rack/session/redis_hash.rb
+++ lib/rack/session/redis_hash.rb
@@ -13,19 +13,14 @@ module Rack
       end
 
       def set_session(env, session_id, session, options)
-        if options[:renew] or options[:drop]
+        if options[:drop]
           session.destroy
           return false if options[:drop]
-          session_id = session.key
         end
-        if session.kind_of?(::RedisHash)
-          session.save
-        elsif session.kind_of?(Hash)
-          final_hash = ::RedisHash.new session_prefix
-          final_hash.update?(new_session)
-          final_hash.save
-          session_id = final_hash.key
+        if options[:renew]
+          session_id = session.renew_key
         end
+        session.save
         return session_id
       end
 

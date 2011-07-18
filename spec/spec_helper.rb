5aa6a8020ada586fb262058710e2870ddb2b1463
Carl Zulauf <carl@lyconic.com>
2011-07-18 01:37:29 -0500

    Made spec look for socket if tcp fails.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'redis/native_hash'
 
+Redis::NativeHash.redis = Redis.new rescue Redis.new(:path => "/tmp/redis.sock")
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
